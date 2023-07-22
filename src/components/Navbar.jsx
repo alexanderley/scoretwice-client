c99852872956b95e28bd214386f1f9746a5e4745
alexanderley <112874444+alexanderley@users.noreply.github.com>
2023-07-22 14:34:23 +0200

    inital commit for the client repo

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,48 @@
+import { Link } from "react-router-dom";
+import { useContext } from "react"; // <== IMPORT
+import { AuthContext } from "../context/auth.context"; // <== IMPORT
+
+function Navbar() {
+  // Subscribe to the AuthContext to gain access to
+  // the values from AuthContext.Provider `value` prop
+  const {
+    isLoggedIn,
+    user, // <== UPDATE
+    logOutUser, // <== UPDATE
+  } = useContext(AuthContext);
+
+  //  Update the rendering logic to display different content
+  //  depending on whether the user is logged in or not
+  return (
+    <nav>
+      <Link to="/">
+        <button>Home</button>
+      </Link>
+
+      {/*    UPDATE     */}
+      {isLoggedIn && (
+        <>
+          <Link to="/projects">
+            <button>Projects</button>
+          </Link>
+          <button onClick={logOutUser}>Logout</button>
+        </>
+      )}
+
+      {!isLoggedIn && (
+        <>
+          <Link to="/signup">
+            {" "}
+            <button>Sign Up</button>{" "}
+          </Link>
+          <Link to="/login">
+            {" "}
+            <button>Login</button>{" "}
+          </Link>
+        </>
+      )}
+    </nav>
+  );
+}
+
+export default Navbar;
