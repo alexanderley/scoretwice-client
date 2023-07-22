5b93d606c8c6ad10ed16bd075542f6d6863e8bcf
alexanderley <112874444+alexanderley@users.noreply.github.com>
2023-07-22 18:55:12 +0200

    changed api address to deployed site

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -5,7 +5,8 @@ import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/auth.context"; // <== IMPORT
 
-const API_URL = "http://localhost:5005";
+// const API_URL = "http://localhost:5005";
+const API_URL = "https://scoretwce-backend.onrender.com/";
 
 function LoginPage(props) {
   const [email, setEmail] = useState("");
