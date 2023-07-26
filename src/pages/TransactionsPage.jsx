e42df7b400606a17a321f3ddcbed4cb74ef4a4b4
Florian Luther <hi@gauvainstudios.com>
2023-07-26 12:22:28 +0200

    deposit done

--- src/pages/TransactionsPage.jsx
+++ src/pages/TransactionsPage.jsx
@@ -13,7 +13,8 @@ const TransactionsPage = () => {
   const [selectedReceiverId, setSelectedReceiverId] = useState("");
   const [senderId, setSenderId] = useState(senderIdFromURL);
   const [userTransactions, setUserTransactions] = useState([]);
-  const storedToken = localStorage.getItem("authToken");
+  const [storedToken, setStoredToken] = useState(localStorage.getItem("authToken"));
+  const [depositAmount, setDepositAmount] = useState(""); // New state to store the deposit amount
 
   const handleAmountChange = (e) => {
     setAmount(e.target.value);
@@ -44,11 +45,46 @@ const TransactionsPage = () => {
       )
       .then((response) => {
         console.log("Transaction successful:", response.data);
-        // Fetch the user's transactions after successful transaction
+        // Fetch the user's transactions after a successful transaction
         fetchUserTransactions();
       })
       .catch((error) => {
         console.error("Error making a new transaction:", error);
+      });
+  };
+
+  const handleDepositChange = (e) => {
+    setDepositAmount(e.target.value);
+  };
+
+  const handleDepositSubmit = (e) => {
+    e.preventDefault();
+    // Call the handleDeposit function with the user ID and the deposit amount
+    handleDeposit(senderId, parseFloat(depositAmount)); // Convert depositAmount to a number (assuming it's a floating-point number)
+    // Clear the deposit amount field after submitting
+    setDepositAmount("");
+  };
+
+  const handleDeposit = (userId, amountToDeposit) => {
+    axios
+      .post(
+        `${API_URL}/api/users/${userId}/account`,
+        {
+          amount: amountToDeposit,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${storedToken}`,
+          },
+        }
+      )
+      .then((response) => {
+        console.log("Deposit successful:", response.data);
+        // Fetch the user's transactions after a successful deposit
+        fetchUserTransactions();
+      })
+      .catch((error) => {
+        console.error("Error making a deposit:", error);
       });
   };
 
@@ -95,106 +131,130 @@ const TransactionsPage = () => {
   }, [storedToken]);
 
   return (
-    <div >
-    <div className="bottomMargin">
-      <form onSubmit={handleSubmit} className="cardContainer">
-      <div >
-        <label className="formContainerLabel">
-          Sender ID:<br></br>
-          <input type="text" value={senderId} readOnly   className="formContainerInput"/>
-        </label>
-        <label className="formContainerLabel">
-          Amount:
+    <div>
+      <div className="bottomMargin">
+        <form onSubmit={handleSubmit} className="cardContainer">
+          <div>
+            <label className="formContainerLabel">
+              Sender ID:<br></br>
+              <input
+                type="text"
+                value={senderId}
+                readOnly
+                className="formContainerInput"
+              />
+            </label>
+            <label className="formContainerLabel">
+              Amount:
+              <br></br>
+              <input
+                type="number"
+                value={amount}
+                onChange={handleAmountChange}
+                placeholder="Enter amount in €"
+                className="formContainerInput"
+              />
+            </label>
+            <label className="formContainerLabel">
+              Bank purpose:<br></br>
+              <input
+                type="text"
+                value={transferMessage}
+                onChange={handleTransferMessageChange}
+                placeholder="Enter the purpose of the transaction"
+                className="formContainerInput"
+              />
+            </label>
+            <label className="formContainerLabel">
+              Receiver:
+              <select
+                value={selectedReceiverId}
+                onChange={handleReceiverChange}
+                className="formContainerInput"
+              >
+                <option value="">Select Receiver</option>
+                {receiverOptions.map((option) => (
+                  <option key={option.value} value={option.value}>
+                    {option.label}
+                  </option>
+                ))}
+              </select>
+            </label>
+          </div>
           <br></br>
-          <input
-            type="number"
-            value={amount}
-            onChange={handleAmountChange}
-            placeholder="Enter amount in €"
-            className="formContainerInput"
-          />
-        </label>
-        <label className="formContainerLabel">
-          Bank purpose:<br></br>
-          <input
-            type="text"
-            value={transferMessage}
-            onChange={handleTransferMessageChange}
-            placeholder="Enter the purpose of the transaction"
-            className="formContainerInput"
-          />
-        </label>
-        <label className="formContainerLabel">
-          Receiver:
-          <select value={selectedReceiverId} onChange={handleReceiverChange}   className="formContainerInput">
-            <option value="">Select Receiver</option>
-            {receiverOptions.map((option) => (
-              <option key={option.value} value={option.value} >
-                {option.label}
-              </option>
-            ))}
-          </select>
-        </label>
-        </div><br></br>
-        <button className="buttonRedNew" type="submit">
-          Make Transaction
-        </button>
-      </form>
+          <button className="buttonRedNew" type="submit">
+            Transaction
+          </button>
+        </form>
+        
 
-
-
-
-
-
+        <div className="cardContainer">
+        <form onSubmit={handleDepositSubmit}>
+          <label className="formContainerLabel">
+            <input
+              type="number"
+              value={depositAmount}
+              onChange={handleDepositChange}
+              placeholder="Enter deposit amount in €"
+              className="formContainerInput"
+            />
+          </label>
+          <button className="buttonRedNew" type="submit">
+            Deposit
+          </button>
+        </form>
+      </div>
 
 
       <div>
-  {userTransactions.length > 0 ? (
-    <ul className="cardContainerInvisible">
-      <h3 className="h3Class">Your transactions:</h3>
-      {userTransactions.map((transaction) => (
-        <li className="cardContainerTransactions" key={transaction._id}>
-          {transaction.sender === senderId ? (
-            <>
-           
-              <strong>sended</strong><br></br>
-              <strong>{transaction.receiver}</strong>
-              <span className="amountpositive">-{transaction.amount}€</span>
-            </>
-          ) : (
-            <>
-            <strong>received</strong><br></br>
-              <strong>{transaction.sender}</strong>
-              <span className="amountnegative">+{transaction.amount}€</span>
-            </>
-          )}
-          <br />
-          <div className="cardContainer">
-          Bank purpose: {transaction.transferMessage}
-          </div>
-         
-          <br />
-        </li>
-      ))}
-    </ul>
-  ) : (
-    <p>No transactions found for the user.</p>
-  )}
-</div>
-</div>
+        {/* Rendering transactions */}
+        {userTransactions.length > 0 ? (
+          <ul className="cardContainerInvisible">
+            <h3 className="h3Class">Your transactions:</h3>
+            {userTransactions.map((transaction) => (
+              <li className="cardContainerTransactions" key={transaction._id}>
+                {transaction.sender === senderId ? (
+                  <>
+                    <strong>sended</strong>
+                    <br></br>
+                    <strong>{transaction.receiver}</strong>
+                    <span className="amountpositive">
+                      -{transaction.amount}€
+                    </span>
+                  </>
+                ) : (
+                  <>
+                    <strong>received</strong>
+                    <br></br>
+                    <strong>{transaction.sender}</strong>
+                    <span className="amountnegative">
+                      +{transaction.amount}€
+                    </span>
+                  </>
+                )}
+                <br />
+                <div className="cardContainer">
+                  Bank purpose: {transaction.transferMessage}
+                </div>
 
+                <br />
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>No transactions found for the user.</p>
+        )}
+      </div>
 
+      </div>
 
+      
 
+      
 
+      
 
-
-
-
-<Footer></Footer>
-
-
-
+      <Footer></Footer>
     </div>
   );
 };
